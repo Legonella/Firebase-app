7fec3de72add2e1ec7ed6f0c6fe2a490df060e9b
Legonella <dastbravis@gmail.com>
2020-12-03 20:41:31 +0100

    React application using databases

--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
 import React from 'react';
+import firebase from 'firebase' 
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCv_YxixaPvYbcD1WaJBm-AcbDbF5g6dAk",
+  authDomain: "fir-app-d8317.firebaseapp.com",
+  databaseURL: "https://fir-app-d8317-default-rtdb.firebaseio.com",
+  projectId: "fir-app-d8317",
+  storageBucket: "fir-app-d8317.appspot.com",
+  messagingSenderId: "329604422842",
+  appId: "1:329604422842:web:a36c003c3d49c8183e7683"
+};
+
+firebase.initializeApp(firebaseConfig);
 
 ReactDOM.render(
   <React.StrictMode>
@@ -11,7 +24,6 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+
