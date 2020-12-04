d5f5f5e5445051ddcc68b1c66a56ba6af75ce4d5
Legonella <dastbravis@gmail.com>
2020-12-04 20:19:22 +0100

    Writing information to the database

--- src/App.js
+++ src/App.js
@@ -2,18 +2,39 @@ import React, { Component } from "react";
 import "./App.css";
 import firebase from "firebase";
 
-
 export default class App extends Component {
-  componentDidMount() {
-    const DB = firebase.database;
-    console.log(DB);
-    }
+	constructor(props) {
+		super(props);
+		this.state = {
+			email: "",
+			password: "",
+		};
+	}
+	componentDidMount() {
+		const DB = firebase.database;
+		console.log(DB);
+	}
+
+	handleChange = ({ target: { value, id } }) => {
+		this.setState({ [id]: value });
+	};
+
 	render() {
 		return (
 			<div>
 				<div className="login_block">
-          <input type="text" placeholder= 'email' />
-					<input type="password" placeholder = 'password' />
+					<input
+						type="text"
+						id="email"
+						placeholder="email"
+						onChange={this.handleChange}
+					/>
+					<input
+						type="password"
+						id="password"
+						placeholder="password"
+						onChange={this.handleChange}
+					/>
 					<input type="submit" />
 				</div>
 			</div>
